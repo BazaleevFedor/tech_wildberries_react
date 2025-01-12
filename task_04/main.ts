5916bf99dfd2dff5adc664afb059c7d2b7869b16
bazaleev.fedor <bazaleev.fedor@wb.ru>
2025-01-12 14:55:16 +0300

    add task_04

--- task_04/main.ts
+++ task_04/main.ts
@@ -7,89 +7,136 @@ enum OrderStatus {
 }
 
 class Order {
-    private readonly _id: string;
-    private readonly _products: Map<Product[], number>;
+    readonly id: string;
 
-    private _status: OrderStatus;
+    private readonly products: Map<Product, number>;
 
-    constructor(newStatus?: OrderStatus, products?: Map<Product[], number>) {
-        this._id = uuidv4();
-        this._status = newStatus ?? OrderStatus.CREATED;
-        this._products = new Map(products);
+    private status: string;
+
+    constructor(products: Map<Product, number>, status?: OrderStatus) {
+        this.id = uuidv4();
+        this.status = status ?? OrderStatus.CREATED;
+        this.products = new Map(products);
     }
 
     getInfo() {
         return {
-            id: this._id,
-            status: this._status,
-            products: this._products,
-        }
+            id: this.id,
+            status: this.status,
+            products: this.products,
+        };
     }
 
     changeStatus(newStatus: OrderStatus) {
-        this._status = newStatus;
+        this.status = newStatus;
     }
 }
 
 class Product {
-    private readonly _id: string;
+    readonly id: string;
 
-    private _name: string;
+    private name: string;
+    private price: number;
 
-    constructor(name: string) {
-        this._id = uuidv4();
-        this._name = name;
+    constructor(name: string, price: number) {
+        this.id = uuidv4();
+        this.name = name;
+        this.price = price;
     }
 
     getInfo() {
         return {
-            id: this._id,
-            name: this._name,
-        }
+            id: this.id,
+            name: this.name,
+            price: this.price,
+        };
     }
 
-    changeInfo(newName: string) {
-        this._name = newName;
+    changeInfo(newName?: string, newPrice?: number) {
+        if (newName) this.name = newName;
+        if (newPrice) this.price = newPrice;
     }
 }
 
 class Cart {
-    private _products: Map<Product, number> = new Map();
+    private products: Map<Product, number> = new Map();
 
     addProduct(newProduct: Product) {
-        const productCount = this._products.get(newProduct);
+        const productCount = this.products.get(newProduct);
         if (productCount) {
-            this._products.set(newProduct, productCount + 1);
+            this.products.set(newProduct, productCount + 1);
 
             return;
         }
 
-        this._products.set(newProduct, 1);
+        this.products.set(newProduct, 1);
+    }
+
+    addProducts(newProducts: Product[]) {
+        newProducts.forEach(this.addProduct.bind(this));
     }
 
     removeProduct(removeProduct: Product) {
-        if (this._products.has(removeProduct)) {
-            this._products.delete(removeProduct);
+        if (this.products.has(removeProduct)) {
+            this.products.delete(removeProduct);
         }
     }
 
     get getProductsList() {
-        return this._products;
+        return this.products;
     }
 }
 
 class OrderManager {
+    private orders: Order[] = [];
 
+    createOrder(products: Map<Product, number>) {
+        const newOrder = new Order(products);
+        this.orders.push(newOrder);
+
+        return newOrder;
+    }
+
+    get getOrdersList() {
+        return this.orders;
+    }
 }
 
 class ProductManager {
-    private _products: Product[];
+    private products: Product[] = [];
 
-    addProductToCart(productID: string) {
+    createProduct(name: string, price: number) {
+        const newProduct = new Product(name, price);
+        this.products.push(newProduct);
 
+        return newProduct;
     }
 
-    getProductInfo(productID: string) {
+    removeProduct(productId: string) {
+        this.products = this.products.filter(product => product.id !== productId);
+    }
 
+    get getProductsList() {
+        return this.products;
     }
 }
+
+// демонстрация
+
+const productManager = new ProductManager();
+const orderManager = new OrderManager();
+const cart = new Cart();
+
+const product1 = productManager.createProduct('часы за сотку', 100);
+const product2 = productManager.createProduct('часы за двести', 200);
+const product3 = productManager.createProduct('лицензия пилота', 1000000);
+
+cart.addProducts([product1, product2, product3, product2]);
+const newOrder = orderManager.createOrder(cart.getProductsList);
+
+console.log(orderManager.getOrdersList);
+
+newOrder.changeStatus(OrderStatus.DELIVERY);
+
+console.log(orderManager.getOrdersList);
+
